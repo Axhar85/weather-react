bd3c0b2042ebd6f4a7919a94c48a9cf5c813cb11
Axhar85 <azhararshad_ntu@yahoo.com>
2021-10-26 11:46:29 +0200

    impletment date

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ function App() {
 
   const dateBuilder = (d) => {
     let months = [
-      "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+      "January", "February", "March", "April", "May", "June",
+       "July", "August", "September", "October", "November", "December"
     ];
     let days = [
-      "Monady", "Tuesday", "Wednesday", "Thruesday", "Friday", "Satuerday", "Sunday"
+      "Monady", "Tuesday", "Wednesday", "Thruesday",
+      "Friday", "Satuerday", "Sunday"
     ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
   }
 
   return (
